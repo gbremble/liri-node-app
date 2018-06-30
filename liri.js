cdb9753a1ac871b2e18f12065ada8b1337c8d0ae
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-06-29 23:53:52 -0400

    added 'spotify-this-song' functionality

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 var twitter = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
-var hr = "=========================###========================="
+var hr = "=========================###=========================\n"
 
 var command = process.argv[2];
 var searchQuery = "";
@@ -18,16 +18,16 @@ var searchQuery = "";
 var nodeArgv = process.argv;
 for (var i = 3; i < nodeArgv.length; i++) {
     if (i > 3 && i < nodeArgv.length) {
-        searchQuery = nodeArgv + "+" + nodeArgv[i];
+        searchQuery = searchQuery + "+" + nodeArgv[i];
     } else {
         searchQuery += nodeArgv[i];
     }
 }
-
+// IF STATEMENT to determine what to do
 if (command === "my-tweets") {
     displayTweets();
 } else if (command === "spotify-this-song") {
-    searchSpotify(searchQuery);
+    searchSpotify();
 } else if (command === "movie-this") {
     // code
 } else if (command === "do-what-it-says") {
@@ -44,12 +44,34 @@ function displayTweets() {
     twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
-                console.log(hr + "\nDate Created: " + JSON.stringify(tweets[i].created_at) + "\n\nTweet:\n" + JSON.stringify(tweets[i].text) + "\n" + hr + "\n")
+                console.log(hr + "Date Created: " + JSON.stringify(tweets[i].created_at) + "\n\nTweet:\n" + JSON.stringify(tweets[i].text) + "\n" + hr)
             }
         }
     });
 }
 
 function searchSpotify() {
-    // code
+    if (searchQuery === "") {
+        searchQuery = "The Sign Ace of Base";
+    }
+    spotify.search({
+        type: 'track',
+        limit: 3,
+        query: searchQuery
+    }, function (error, data) {
+        console.log("Searching Spotify for " + searchQuery + "...\n")
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+        // var data = data.tracks.items;
+
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            console.log(hr + "Artist: " + data.tracks.items[i].artists[0].name);
+            console.log("Song Title: " + data.tracks.items[i].name);
+            console.log("Preview Link: " + data.tracks.items[i].preview_url);
+            console.log("Album Title: " + data.tracks.items[i].album.name);
+            console.log(hr);
+        }
+        console.log("\n*=================* END OF DATA *==================*\n");
+    });
 }
